1b939e817554c096843d7a7886f043131a76c3e7
nhathdit <nhathdqt@gmail.com>
2023-06-28 11:16:11 +0700

    update ddt source to see red alert

--- src/test/java/org/hdn/mathutil/test/MathUtilityAdvancedTest.java
+++ src/test/java/org/hdn/mathutil/test/MathUtilityAdvancedTest.java
@@ -31,7 +31,7 @@ public class MathUtilityAdvancedTest {
                         {1, 1},
                         {2, 2},
                         {5, 120},
-                        {6, 720}
+                        {6, 7200}
                 };
         return testData;
     }
